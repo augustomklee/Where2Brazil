050fd0575b43f2af1a3c25c4041409e460c5bb7a
Augusto MK Lee <augusto.mk.lee@gmail.com>
2025-09-19 18:25:37 -0300

    removed /public/ from relative path for logo.png

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -40,7 +40,7 @@ export default function QuizPage(props) {
 
   return (
     <section className="quiz-page">
-        <img src="/public/logo.png" alt="Where2Brazil Logo" className="logo" />
+        <img src="/logo.png" alt="Where2Brazil Logo" className="logo" />
         <h2>{question.question}</h2>
         <form className="question-form" action={handleForm}>
             {question.options.map((option) => (
